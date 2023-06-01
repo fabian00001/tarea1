5896ba0a6bf2acfa1921ad89fd95e86b89c5a6b8
Fabian <Fabian@DESKTOP-G96D5G6>
2023-05-31 22:40:42 -0600

    Agregar archivos de proyecto.

---
+++ Clase1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            char Opcion = 's';
+            do
+            {
+                int num1;
+                int num2;
+                int suma;
+                int resta;
+                int multi;
+                int div;
+
+
+                
+                Console.WriteLine("1- Sumar");
+                Console.WriteLine("2- Restar");
+                Console.WriteLine("3- Dividir");
+                Console.WriteLine("4- Multiplicar");
+                Console.WriteLine("5 salir del menu");
+                Opcion = char.Parse(Console.ReadLine());
+
+                switch (Opcion)
+                {
+                    case '1':
+                        Console.WriteLine("ingrese el primer numero: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("ingrese el segundo numero: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        suma = num1 + num2;
+                        Console.WriteLine(num1+ " + " + num2 + " = " + suma);
+                        break;
+                    case '2':
+
+                        Console.WriteLine("ingrese el primer numero: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("ingrese el segundo numero: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        resta = num1 - num2;
+                        Console.WriteLine(num1 + " - " + num2 + " = " + resta);
+                        break;
+                    case '3':
+                        Console.WriteLine("ingrese el primer numero: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("ingrese el segundo numero: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        multi = num1 * num2;
+                        Console.WriteLine(num1 + " x " + num2 + " = " + multi);
+                        break;
+                    case '4':
+                        Console.WriteLine("ingrese el primer numero: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("ingrese el segundo numero: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        div = num1 / num2;
+                        Console.WriteLine(num1 + " ÷ " + num2 + " = " + div);
+                        break;
+                    case '5':
+                        Console.WriteLine("Ha salido del menu"); break;
+                        
+                    default: Console.WriteLine("opcion incorrecta"); break;
+
+                }
+
+            } while (!Opcion.Equals('5'));
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+
+}
+
---
+++ clase1upi/clase1upi/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase1upi
+{
+    internal class Program
+    {
+      static  float resultado = 0; // variable global
+        static float v1, v2;
+        static void Main(string[] args)  // metodo 
+        {
+            menu();
+            Console.ReadKey();
+        }
+
+        public static void SolicitarDatos()
+        {
+            Console.WriteLine("Digite un valor: ");
+             v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite otro valor: ");
+             v2 = float.Parse(Console.ReadLine());
+        }
+        public static void menu()
+        {
+            resultado = 0;
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("1- suma");
+                Console.WriteLine("2- resta");
+                Console.WriteLine("3- division");
+                Console.WriteLine("4- multiplicacion");
+                Console.WriteLine("5- Salir");
+                Console.WriteLine("Digite una opcion: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:SolicitarDatos();
+                          suma(v1, v2); break;
+                    case 2:
+                        SolicitarDatos();
+                        Console.WriteLine("el resultado de la resta es: {0}", resta(v1,v2));
+                        break;
+                    case 3:
+                        SolicitarDatos();
+                        division(v1,v2);
+                        break;
+                    case 4:
+                        SolicitarDatos();
+                        Console.WriteLine("La mulplicaion es: {0}", multiplicacion(v1, v2));
+                        break;
+                    case 5: break;
+
+
+                    default:
+                        break;
+                }
+
+            } while (opcion != 3);
+        }
+
+        public static void division(float valor1, float valor2)
+        {
+            resultado = 0;
+            float total = valor1 / valor2;   // variable locales
+            Console.WriteLine($"La division es : {total}");
+        }
+
+        public static float multiplicacion(float valor1, float valor2)
+        {
+            float total = valor1 * valor2;
+            return total;
+           
+        }
+        public static void suma(float valor1, float valor2)
+        {
+            float total = valor1 + valor2;
+            int sumar; // variable locales
+
+            Console.WriteLine($"la suma es de: {total}");
+        }
+        public static float resta(float valor1, float valor2)
+        {
+            float total = valor1 - valor2;
+            return total;
+        }
+    }
+}
